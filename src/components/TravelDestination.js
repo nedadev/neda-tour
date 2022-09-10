f9326bef79cbbd9fe2c333243782d85930a4be2f
Neda A <nazizi1988@gmail.com>
2022-09-10 19:15:31 +0200

    destination info and components completed

---
+++ src/components/TravelDestination.js
@@ -0,0 +1,52 @@
+import ReactCountryFlag from "react-country-flag";
+
+const GetCountry = (props) => {
+  const { code, name } = props;
+  return (
+    <div style={{ display: "flex" }}>
+      <ReactCountryFlag
+        countryCode={code}
+        svg
+        style={{
+          width: "1.6em",
+          height: "1.6em",
+          marginRight: "1rem",
+        }}
+        title={code}
+      />
+      <h3>{name}</h3>
+    </div>
+  );
+};
+
+const TravelDestination = (props) => {
+  return (
+    <article className="travel-destination">
+      <h2>Destination</h2>
+      <h1>Rome</h1>
+      <div>
+        <div>
+          <GetCountry name={"Netherlands"} code={"NL"} />
+          <span
+            className="material-symbols-rounded"
+            style={{ margin: "0 1rem" }}
+          >
+            sync_alt
+          </span>
+          <GetCountry name={"Italy"} code={"IT"} />
+        </div>
+        <div>
+          <span
+            class="material-symbols-rounded"
+            style={{ marginRight: "1rem" }}
+          >
+            flight
+          </span>
+          <h3>2 h 25 min flight</h3>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default TravelDestination;
